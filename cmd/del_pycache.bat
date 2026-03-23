f439bd6b63f7aaa1fc12c2208567765c446e45c1
Arendelle <Arendelle_@outlook.com>
2026-03-23 20:50:20 +0800

    remove cmd/

--- cmd/del_pycache.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-for /d /r "physicsLab" %%d in (__pycache__) do (
-    if exist "%%d" (
-        echo Deleting %%d
-        rd /s /q "%%d"
-    )
-)
--- cmd/del_pycache.sh
+++
@@ -1,3 +0,0 @@
-# ./cmd/del_pycache.sh
-find ./physicsLab -type d -name "__pycache__" -exec echo "Deleting {}" \;
-find ./physicsLab -type d -name "__pycache__" -exec rm -rf {} \; 2>/dev/null
